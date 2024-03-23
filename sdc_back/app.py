07a1703c380db18500a072af6a76acd9637e407f
Javantax <79591148+Javantax@users.noreply.github.com>
2024-03-23 11:18:16 -0300

    Fetch JSON file depending on endpoint

--- sdc_back/app.py
+++ sdc_back/app.py
@@ -7,23 +7,25 @@ app = Flask(__name__)
 parent_directory = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
 json_files = {
-    "agriculture": "CSVfiles/Agriculture_dat.json",
-    "aviation": "CSVfiles/file2.json",   
+    "agriculture": os.path.join(parent_directory, "CSVfiles/Agriculture_dat.json"),
+    "aviation": os.path.join(parent_directory, "CSVfiles/file2.json"),   
 }
 
 @app.route('/')
 def get_agriculture_json():
+    
     # Check if file exists in the json_files dictionary
     if 'agriculture' in json_files:
-        # Corresponding JSON file path
+        
         json_file_path = json_files['agriculture']
+        print(f"File path: {json_file_path}")  # Print the file path
 
         # If file exists
         if os.path.exists(json_file_path):
             # Load the JSON data from file
             with open(json_file_path, 'r') as file:
                 json_data = json.load(file)
-            
+                
             return jsonify(json_data)
         else:
             return jsonify({'error': 'File not found'}), 404
