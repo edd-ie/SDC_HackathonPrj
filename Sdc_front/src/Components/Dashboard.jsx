fd79b676afc888765b813ca13927ddee92aae9a2
edd-ie <ogendoeddie@gmail.com>
2024-03-23 15:38:18 -0300

    Future predictions

--- Sdc_front/src/Components/Dashboard.jsx
+++ Sdc_front/src/Components/Dashboard.jsx
@@ -8,70 +8,8 @@ import './Dashboard.css'
 
 export default function Dashboard() {
     const [data, setData] = useState([])
-    const [prev, active] = useState()
-
-    const dataset = [
-        {
-            "name": "Agriculture",
-            "uv": 4000,
-            "pv": 2400,
-            "amt": 2400
-        },
-        {
-            "name": "Aviation",
-            "uv": 3000,
-            "pv": 1398,
-            "amt": 2210
-        },
-        {
-            "name": "Commercial",
-            "uv": 2000,
-            "pv": 9800,
-            "amt": 2290
-        },
-        {
-            "name": "Energy",
-            "uv": 2780,
-            "pv": 3908,
-            "amt": 2000
-        },
-        {
-            "name": "Forestry",
-            "uv": 1890,
-            "pv": 4800,
-            "amt": 2181
-        },
-        {
-            "name": "Industrial",
-            "uv": 2390,
-            "pv": 3800,
-            "amt": 2500
-        },
-        {
-            "name": "Marine",
-            "uv": 3490,
-            "pv": 4300,
-            "amt": 2100
-        },
-        {
-            "name": "Residential",
-            "uv": 3490,
-            "pv": 4300,
-            "amt": 2100
-        },
-        {
-            "name": "Transportation",
-            "uv": 3490,
-            "pv": 4300,
-            "amt": 2100
-        },
-        {
-            "name": "Waste",
-            "uv": 3490,
-            "pv": 4300,
-            "amt": 2100
-        }
-    ]
+    const [flip, setFlip] = useState(false)
+    const [future, setFuture] = useState(true)
 
     function handleData(e) {
         
@@ -118,11 +56,59 @@ export default function Dashboard() {
         })
     },[])
 
+    function handleShift(e) {
+        let button = e.target;
+        let name = button.innerText;
+        let idName = button.id;
+        let parent = button.parentElement;
+        
+        setFlip(idName!="visual" ? true : false)
+        if(idName =="visual"){
+           fetch(`http://localhost:5000/sector/total`)
+            .then(res => res.json())
+            .then(data => {
+
+                const dataArray = Object.entries(data).map(([name, value]) => ({
+                    name,
+                    uv: value.emission,
+                    pv: value.offset
+                    })
+                );
+                console.log(dataArray);
+                setData(dataArray);
+            })
+        }
+        
+        for (let i = 0; i < parent.children.length; i++) {
+            parent.children[i].className = "bars"
+        }
+        console.log(name, button.className, idName);
+        button.className = "bars activeBar"
+
+        setFuture(idName=="pred" ? true : false)
+        if(idName == "pred"){
+            fetch(`http://localhost:5000/prediction`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                const dataArray = Object.entries(data).map(([name, value]) => ({
+                    name,
+                    uv: value.toFixed(2)
+                    })
+                );
+                console.log(dataArray);
+                setData(dataArray);
+            })
+        }
+    }
+
 
 
 
     return (
     <div className="Dashboard">
+        
+        {!flip ? 
         <div className="folder">
             <AreaChart className="chart" width={700} height={250} data={data}
             margin={{ top: 10, right: 0, left: 0, bottom: 0 }}>
@@ -156,6 +142,49 @@ export default function Dashboard() {
                 <div id="residential" className="switch" onClick={handleData} >Residential</div>
                 <div id="transportation" className="switch" onClick={handleData} >Transportation</div>
                 <div id="waste" className="switch" onClick={handleData} >Waste</div>
+            </div>
+        </div>
+        : 
+            <div className="folder">
+                {future ? 
+                    <AreaChart className="chart" width={700} height={250} data={data}
+                        margin={{ top: 10, right: 0, left: 0, bottom: 0 }}>
+                        <defs>
+                            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                            <stop offset="5%" stopColor="#8884d8" stopOpacity={0.9}/>
+                            <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
+                            </linearGradient>
+                            <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                            <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8}/>
+                            <stop offset="95%" stopColor="#82ca9d" stopOpacity={0}/>
+                            </linearGradient>
+                        </defs>
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <Tooltip />
+                        <Area type="monotone" dataKey="uv" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
+                        <Area type="monotone" dataKey="pv" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
+                    </AreaChart>
+                : <div></div>}
+            </div>
+        }
+
+        <div className="sidebar">
+            <div className="bars" id="visual" onClick={handleShift}>
+                <span className="material-symbols-sharp">
+                monitoring
+                </span> Visualize
+            </div>
+            <div className="bars" id="pred" onClick={handleShift}>
+                <span className="material-symbols-sharp">
+                emoji_objects
+                </span> Predictions
+            </div>
+            <div className="bars" id="invest" onClick={handleShift}>
+                <span className="material-symbols-sharp">
+                monetization_on
+                </span> Investments
             </div>
         </div>
         
