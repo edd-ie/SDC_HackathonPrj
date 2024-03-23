c7308f82412e1f3330bd276fcdf6c7083b17a691
Harsha <harsha.emani987@gmail.com>
2024-03-23 11:42:28 -0300

    Importing data from sector.json

--- sdc_back/app.py
+++ sdc_back/app.py
@@ -6,31 +6,34 @@ app = Flask(__name__)
 
 parent_directory = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
-json_files = {
-    "agriculture": os.path.join(parent_directory, "CSVfiles/Agriculture_dat.json"),
-    "aviation": os.path.join(parent_directory, "CSVfiles/file2.json"),   
-}
+
+jsonFilePath = os.path.join(parent_directory, "CSVfiles/sector.json")
+
+def loadJsonFiles(sector):
+    # If file exists
+    print(jsonFilePath)
+    if os.path.exists(jsonFilePath):
+        # Load the JSON data from file
+        with open(jsonFilePath, 'r') as file:
+            json_data = json.load(file)
+            agriculture = json_data[sector]
+            # print(agriculture)
+                
+        return jsonify(agriculture)
+    else:
+        return jsonify({'error': 'File not found'}), 404
 
 @app.route('/')
-def get_agriculture_json():
-    
-    # Check if file exists in the json_files dictionary
-    if 'agriculture' in json_files:
-        
-        json_file_path = json_files['agriculture']
-        print(f"File path: {json_file_path}")  # Print the file path
+def index():
+    return "This is the base route"
 
-        # If file exists
-        if os.path.exists(json_file_path):
-            # Load the JSON data from file
-            with open(json_file_path, 'r') as file:
-                json_data = json.load(file)
-                
-            return jsonify(json_data)
-        else:
-            return jsonify({'error': 'File not found'}), 404
-    else:
-        return jsonify({'error': 'Invalid endpoint'}), 400
+@app.route('/<sector>')
+def get_agriculture_json(sector):
+    # Check if file exists in the json_files dictionary
+    # return loadJsonFiles("agriculture")
+    if(sector != "agriculture" and sector != "commercial" and sector != "energy" and sector != "aviation" and sector != "forestry" and sector != "industrial" and sector != "marine" and sector != "residential" and sector != "transporation" and sector != "waste"):
+        return "Invalid route"
+    return loadJsonFiles(sector)
 
 if __name__ == "__main__":
     app.run(debug=True)
